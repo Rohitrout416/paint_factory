639d2b0b8c28f6e864967b575df9f26ca4722773
Rohitrout416 <79717560+Rohitrout416@users.noreply.github.com>
2023-06-14 00:44:03 +0530

    added a button to be able to contact from the bottom of the website

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,28 +1,39 @@
-import React from "react";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import React, { useState } from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Button } from '@chakra-ui/react';
 import Heading from './Heading';
 import About from './About';
 import Contacts from './Contacts';
 
 function HomePage(){
 
+    const [tabIndex, setTabIndex] = useState(0);
+
     return (
     <div className="app-container">
-      <Tabs variant='enclosed' className="tabs-container">
+      <Tabs variant='enclosed' className="tabs-container" index={tabIndex}>
         <TabList className="tab-list">
           <Heading className="heading" />
-          <Tab className="tab">About</Tab>
-          <Tab className="tab">Contact</Tab>
+          <Tab className="tab" onClick={()=>setTabIndex(0)}>About</Tab>
+          <Tab className="tab" onClick={()=>setTabIndex(1)}>Contact</Tab>
         </TabList>
         <TabPanels className="tab-panels">
           <TabPanel className="tab-panel">
             <About />
+            <Button w="100%" colorScheme="blue" size="lg" onClick={()=>setTabIndex(1)}>
+              Contact Us Today!
+            </Button>
           </TabPanel>
           <TabPanel className="tab-panel">
             <Contacts />
+            <div className="centered-container">
+              <Button w="50%" colorScheme="green" onClick={() => setTabIndex(0)}>
+                Thank You!
+              </Button>
+          </div>
           </TabPanel>
         </TabPanels>
       </Tabs>
+
     </div>
     );
 }
